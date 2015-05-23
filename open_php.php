c657c286df42206c6c9e00b3930884faecafa625
sdj38 <sjones38@live.com>
2015-05-22 21:04:49 -0600

    open php

--- open_php.php
+++ open_php.php
@@ -3,13 +3,13 @@
 	$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 	$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 	$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
-	$dbname = "web";
+	$dbname = "sandwich_db";
 	// Create connection
 	#$servername = 'localhost';
 	#$user = 'sdj';
 	#$password = 'password';
 	#$dbname='sandwich_db';
-echo "host:$servername:$port dbname:$dbname user:$username password:$password<br >\n";
+//echo "host:$servername:$port dbname:$dbname user:$username password:$password<br >\n";
 	try{
 	$conn = new PDO("mysql:host=$servername:$port;dbname=$dbname", $username, $password);
 	}
