321e8faa3cdbc7a9714943afd6eff12a72afb0b8
sdj38 <sjones38@live.com>
2015-05-23 11:44:10 -0600

    open php

--- open_php.php
+++ open_php.php
@@ -1,38 +1,12 @@
 <?php
 
-	
-$dbHost = "";
-  $dbPort = "";
-  $dbUser = "";
-  $dbPassword = "";
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+$dbname = "web";
 
-     $dbName = 'web';
-
-     $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-
-     if ($openShiftVar === null || $openShiftVar == "")
-     {
-          // Not in the openshift environment
-          //echo "Using local credentials: "; 
-        $dbHost = 'localhost';
-	$dbUser = 'sdj';
-	$dbPassword = 'password';
-	$dbname='sandwich_db';
-     }
-     else 
-     { 
-          // In the openshift environment
-          echo "Using openshift credentials: ";
-
-          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-          $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
-          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-     } 
-     echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
-
-     $conn = new PDO('mysql:host='.$dbHost.';dbname='.$dbName, $dbUser, $dbPassword);
-	
-	
+$dsn = 'mysql:dbname='.$dbname.';host='.DB_HOST.';port='.DB_PORT;
+$conn = new PDO($dsn, DB_USER, DB_PASS);
 
 ?>
