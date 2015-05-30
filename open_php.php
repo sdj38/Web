0b66c5f474d2176b4683b1085f5950083cc05f14
sdj38 <sjones38@live.com>
2015-05-30 11:20:41 -0600

    Update open_php.php

--- open_php.php
+++ open_php.php
@@ -4,7 +4,7 @@ define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-$dbname = "web";
+$dbname = "sandwich_db";
 
 $dsn = 'mysql:dbname='.$dbname.';host='.DB_HOST.';port='.DB_PORT;
 $conn = new PDO($dsn, DB_USER, DB_PASS);
